fa5da39f68dcd72b65fb59a57df4b4d566af3d18
Bérénice BRUOT <bebe97270@gmail.com>
2021-12-31 15:02:36 +0100

    � NEW: Initial Commit

--- class.php
+++ class.php
@@ -1,10 +1,18 @@
 <?php
 
 class Crêpes {
-    public $taille ="Medium";
-    public $forme ="ronde";
-    public $ingredients =array("oeufs","lait","beurre","farine","fleur d'oranger","rhum");
+    public $oeufs ="2 oeufs";
+    public $lait ="40 cl de lait";
+    public $beurre ="20g de beurre";
+    public $farine ="50g de farine";
+    public $rhum ="10 cl de rhum";
+
 }
 
-$Crêpes  = new Crêpes();
-?>
+// Afficher propriété de classe  => afficher-recette
+
+function afficher_recette () {
+    echo 'Pour ma recette de crêpes, il me faut:' . ($oeufs) . ($lait) . ($beurre) . ($farine) . ($rhum)
+}
+
+?> 
