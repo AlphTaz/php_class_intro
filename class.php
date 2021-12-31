39ec280ccc8a2fe303830270224a9b4956b1742a
Bérénice BRUOT <bebe97270@gmail.com>
2021-12-31 14:32:30 +0100

    � NEW: Initial Commit

---
+++ class.php
@@ -0,0 +1,10 @@
+<?php
+
+class Crêpes {
+    public $taille ="Medium";
+    public $forme ="ronde";
+    public $ingredients =array("oeufs","lait","beurre","farine","fleur d'oranger","rhum");
+}
+
+$Crêpes  = new Crêpes();
+?>
